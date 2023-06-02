b6b6e161a98b405877c6b481de41ea7a96c8e378
王高敏 <wanggaomin@greatmap.com.cn>
2023-06-02 15:21:51 +0800

    store核心getters的使用

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,15 @@ import { createStore} from 'vuex'
 
 const store = createStore({
   state:()=> ({
-    count: 0,
+    count: 10,
     name: 'wanggaomin',
     level: 100,
   }),
+  getters: {
+    doubleCount(state){
+      return state.count*2
+    }
+  },
   // vuex早期需要用mutations进行修改
   mutations:{
     increment(state){
---
+++ src/views/02_store核心getters的使用.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 这里默认也能使用vue的this.语法 -->
+    <h1>{{ $store.getters.doubleCount }}</h1>
+
+    <!-- 2、使用compositions api/vue3语法 -->
+    <h1>compositions api显示getters:{{ store.getters.doubleCount }}</h1>
+  </div>
+</template>
+
+<script setup>
+  import { useStore } from 'vuex';
+  const store = useStore()
+</script>
+
+<style>
+
+</style>
