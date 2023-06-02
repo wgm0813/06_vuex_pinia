520837ac4a0e215340e4f57c8dd7cc4226bbce36
王高敏 <wanggaomin@greatmap.com.cn>
2023-06-02 10:11:22 +0800

    options api里面使用

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,31 @@
   <div>
     <h1>Vue3</h1>
     <!-- store中的counter -->
-    <h2>当前计数{{ $store.state.count }}</h2>
+    <h2>compositions api当前计数{{ $store.state.count }}</h2>
+    <h2>options api当前计数{{ storeCount }}</h2>
+    <button @click="increment">+1</button>
   </div>
 </template>
 
+<!-- 如果是在options api里面写在computed里面然后使用较好 -->
+<script>
+export default {
+  computed:{
+    storeCount() {
+      return this.$store.state.count
+    }
+  }
+}
+</script>
 <script setup>
   // import { ref } from "vue";
 
   // const count = ref(100)
+  import { useStore } from 'vuex';
+  const store = useStore()
+  function increment(){
+    store.commit('increment')
+  }
 </script>
 
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,13 @@ import { createStore} from 'vuex'
 const store = createStore({
   state:()=> ({
     count: 0
-  })
+  }),
+  // vuex早期需要用mutations进行修改
+  mutations:{
+    increment(state){
+      state.count++
+    }
+  }
 })
 
 export default store
