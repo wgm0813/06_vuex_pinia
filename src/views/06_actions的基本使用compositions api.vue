f31b76af27307b9fbc07371dd84bac45e9906182
王高敏 <wanggaomin@greatmap.com.cn>
2023-06-05 09:24:10 +0800

    actions两种api分开文件展示

--- src/views/06_actions的基本使用compositions api.vue
+++ src/views/06_actions的基本使用compositions api.vue
@@ -1,24 +1,11 @@
 <template>
   <div>
-    <h1>ations的基本使用</h1>
-    <!-- options api写法 -->
-    <!-- <h2>当前计数：{{ $store.state.count }}</h2> -->
-
-    <!-- compositions api写法 -->
+    <h1>ations的基本使用compositions api</h1>
     <h2>当前计数：{{ store.state.count }}</h2>
     <button @click="actionClick">发起actions</button>
   </div>
 </template>
 
-<!-- <script>
-export default {
-  methods: {
-    actionClick(){
-      this.$store.dispatch('increment')
-    }
-  }
-}
-</script> -->
 <script setup>
   import { useStore } from 'vuex';
   const store = useStore();
---
+++ src/views/06_actions的基本使用options api.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>ations的基本使用options api</h1>
+    <!-- options api写法 -->
+    <h2>当前计数：{{ $store.state.count }}</h2>
+    <button @click="actionClick">发起actions</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    actionClick(){
+      this.$store.dispatch('increment')
+    }
+  }
+}
+</script>
