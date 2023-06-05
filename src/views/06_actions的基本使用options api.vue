6521ad207467848214219db180fdc36593b31c25
王高敏 <wanggaomin@greatmap.com.cn>
2023-06-05 10:02:26 +0800

    mapActions辅助函数

--- src/views/06_actions的基本使用options api.vue
+++ src/views/06_actions的基本使用options api.vue
@@ -3,16 +3,20 @@
     <h1>ations的基本使用options api</h1>
     <!-- options api写法 -->
     <h2>当前计数：{{ $store.state.count }}</h2>
-    <button @click="actionClick">发起actions</button>
+    <!-- <button @click="actionClick">发起actions</button> -->
+    <button @click="increment">发起actions</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   methods: {
-    actionClick(){
-      this.$store.dispatch('increment')
-    }
+    // 使用辅助函数
+    ...mapActions(['increment']),
+    // actionClick(){
+    //   this.$store.dispatch('increment')
+    // }
   }
 }
 </script>
